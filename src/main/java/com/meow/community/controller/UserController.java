6f62c6afc15c213d1e725a05ecab6d407e097519
WangMeow1998 <wangmeow1998@163.com>
2022-04-11 16:03:15 +0800

    账号设置-上传头像/修改密码

---
+++ src/main/java/com/meow/community/controller/UserController.java
@@ -0,0 +1,114 @@
+package com.meow.community.controller;
+
+import com.meow.community.entity.User;
+import com.meow.community.service.UserService;
+import com.meow.community.util.CommunityUtil;
+import com.meow.community.util.HostHolder;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping(path = "/user")
+public class UserController {
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${community.path.domain}")
+    private String domin;
+
+    @Value("${community.path.upload}")
+    private String uploadPath;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(path = "/setting", method = RequestMethod.GET)
+    public String getUserSettingPage(Model model){
+        return "/site/setting";
+    }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public String uploadHeader(MultipartFile headerImage, Model model){
+        if(headerImage == null){
+            model.addAttribute("error","您还没有选择图片!");
+            return "/site/setting";
+        }
+        String fileName = headerImage.getOriginalFilename();
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        if(StringUtils.isBlank(suffix)){
+            model.addAttribute("error","文件的格式不正确");
+            return "/site/setting";
+        }
+        //为了防止头像冲突，为用户上传的头像文件生成随机的文件名
+        fileName = CommunityUtil.generateUUID() + suffix;
+        //确定文件存放的路径
+        File dest = new File(uploadPath + "/" + fileName);
+        try {
+            headerImage.transferTo(dest);
+        } catch (IOException e) {
+            logger.error("上传文件失败: " + e.getMessage());
+            throw new RuntimeException("上传文件失败，服务器发生异常",e);
+        }
+        //更新当前用户的头像路径（Web访问路径）
+        //格式：http://localhost:8080/community/user/header/xxx.png
+        User user = hostHolder.getUser();
+        String headerUrl = domin + contextPath + "/user/header/" + fileName;
+        userService.updateHeader(user.getId(), headerUrl);
+
+        return "redirect:/index";
+    }
+
+    @RequestMapping(path = "/header/{fileName}", method = RequestMethod.GET)
+    public void getHeader(@PathVariable("fileName") String fileName, HttpServletResponse response){
+        //服务器存放头像的路径
+        String fileNamePath = uploadPath + "/" + fileName;
+        //文件后缀
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        //响应头像图片
+        response.setContentType("image/" + suffix);
+
+        try (   FileInputStream fis = new FileInputStream(fileNamePath);
+                OutputStream os = response.getOutputStream();
+        ){
+            byte[] buffer = new byte[1024];
+            int b = 0;
+            while((b = fis.read(buffer)) != -1){
+                os.write(buffer,0,b);
+            }
+        } catch (IOException e) {
+            logger.error("读取头像失败: " + e.getMessage());
+        }
+    }
+    @RequestMapping(path = "/updatePassword", method = RequestMethod.POST)
+    public String changeUserPassword(String oldPassword, String newPassword, Model model){
+        //获取用户
+        User user = hostHolder.getUser();
+        Map<String, Object> map = userService.updatePassword(user.getId(), oldPassword, newPassword);
+        if(map == null || map.isEmpty()){
+            return "redirect:/logout";
+        }else{
+            model.addAttribute("oldPasswordMsg",map.get("oldPasswordMsg"));
+            model.addAttribute("newPasswordMsg",map.get("newPasswordMsg"));
+            return "/site/setting";
+        }
+    }
+}
--- src/main/java/com/meow/community/service/UserService.java
+++ src/main/java/com/meow/community/service/UserService.java
@@ -162,4 +162,31 @@ public class UserService implements CommunityConstant {
     public LoginTicket getLoginTicket(String ticket){ //根据当前凭证查询登录凭证记录
         return loginTicketMapper.selectByTicket(ticket);
     }
+
+    public int updateHeader(int userId, String headerUrl){ //更新用户头像的路径
+        return userMapper.updateHeader(userId, headerUrl);
+    }
+    public Map<String, Object> updatePassword(int userId, String oldPassword, String newPassword){
+        Map<String, Object> map = new HashMap<>();
+        //空值处理
+        if(StringUtils.isBlank(oldPassword)){
+            map.put("oldPasswordMsg","原始密码不可为空!");
+            return map;
+        }
+        if(StringUtils.isBlank(newPassword)){
+            map.put("newPasswordMsg","新密码不可为空!");
+            return map;
+        }
+        //验证原始密码
+        User user = userMapper.selectById(userId);
+        oldPassword = CommunityUtil.md5(oldPassword + user.getSalt());
+        if(!user.getPassword().equals(oldPassword)){
+            map.put("oldPasswordMsg","原始密码不正确!");
+            return map;
+        }
+        //更新密码
+        newPassword = CommunityUtil.md5(newPassword + user.getSalt());
+        userMapper.updatePassword(userId, newPassword);
+        return map;
+    }
 }
